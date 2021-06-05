c6fc9ee5bcb61279e4adcf731a1759cfbf457055
ngantk23 <trankhanhngan00@gmail.com>
2021-06-05 09:03:44 +0700

    d

--- src/store/reducer/ReducerCart.tsx
+++ src/store/reducer/ReducerCart.tsx
@@ -18,22 +18,23 @@ const initialState: CartType[] = [
 ]
 
 export const ReducerCart: Reducer<CartType[], CartAction> = (state = initialState, actions: CartAction) => {
+    console.log('khj', actions.type);
     switch (actions.type) {
         case types.ADD_TO_CART: {
             let newList = state;
             let exitInCart = false;
             newList.map(item => {
-                if (item.id === actions.payload){
+                if (item.id === actions.payload) {
                     item.quantity += 1;
                     exitInCart = true;
-                } 
+                }
             })
-            if(!exitInCart) 
-                newList=[
+            if (!exitInCart)
+                newList = [
                     ...newList,
                     {
                         id: actions.payload,
-                        quantity:1
+                        quantity: 1
                     }
                 ]
 
@@ -43,30 +44,30 @@ export const ReducerCart: Reducer<CartType[], CartAction> = (state = initialStat
 
         case types.REMOVE_FROM_CART: {
             let newList = state;
-            const index = newList.findIndex(item => item.id===actions.payload);
-            newList.splice(index,1);
+            const index = newList.findIndex(item => item.id === actions.payload);
+            newList.splice(index, 1);
             return newList;
         }
 
         case types.INCREASE_IN_CART: {
             let newList = state;
-            const index = newList.findIndex(item => item.id===actions.payload);
-            newList[index]={
+            const index = newList.findIndex(item => item.id === actions.payload);
+            newList[index] = {
                 ...newList[index],
-                quantity : newList[index].quantity+1
+                quantity: newList[index].quantity + 1
             }
             return newList;
         }
 
         case types.DECREASE_IN_CART: {
             let newList = state;
-            const index = newList.findIndex(item => item.id===actions.payload);
-            if(newList[index].quantity===1) newList.splice(index,1);
-            else 
-            newList[index]={
-                ...newList[index],
-                quantity : newList[index].quantity-1
-            }
+            const index = newList.findIndex(item => item.id === actions.payload);
+            if (newList[index].quantity === 1) newList.splice(index, 1);
+            else
+                newList[index] = {
+                    ...newList[index],
+                    quantity: newList[index].quantity - 1
+                }
             return newList;
         }
 
